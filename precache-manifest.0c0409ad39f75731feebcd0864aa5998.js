9974e1cea5951fb5bdb87b9fadf454f6e4b709c9
wanyanyan <wanyanyan528@gmail.com>
2019-11-28 17:00:17 +0800

    first commit

---
+++ precache-manifest.0c0409ad39f75731feebcd0864aa5998.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997b66bf5dda4187f63516787f98ec6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed9d250559121d4a76f3",
+    "url": "/static/css/main.de63c54f.chunk.css"
+  },
+  {
+    "revision": "310192c8e207e8bff2bc",
+    "url": "/static/js/2.94e7c646.chunk.js"
+  },
+  {
+    "revision": "ed9d250559121d4a76f3",
+    "url": "/static/js/main.4aff5026.chunk.js"
+  },
+  {
+    "revision": "34b61dc97636fc0f8db2",
+    "url": "/static/js/runtime~main.39178213.js"
+  }
+]);
